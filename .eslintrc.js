362677626955e757dc59c29f22325cc0286effdf
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2020-11-03 01:05:12 +0000

    root import lib

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,19 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      'babel-plugin-root-import': {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
